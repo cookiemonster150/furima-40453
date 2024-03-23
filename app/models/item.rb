0c2a365ac13119542f329d84bb0c9665e9797740
cookiemonster150 <160557231+cookiemonster150@users.noreply.github.com>
2024-03-23 15:52:26 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,7 +17,7 @@ class Item < ApplicationRecord
   validates :postage_id,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :day_id,presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, allow_nil: true
+  validates :price,presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }, allow_nil: true
   validates :image, presence: true
 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,61 +1,63 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  before do
+    @item = FactoryBot.build(:item)
+  end
   describe 'バリデーション' do
     context '商品出品できる場合' do
       it "item_name,description,category_id,condition_id,postage_id,prefecture_id,day_id" do
-       item = FactoryBot.create(:item) # テスト用のitemオブジェクトを作成する
-       expect(item).to be_valid
+       expect(@item).to be_valid
       end
     end
    context '商品が出品できない' do
     it "item_nameが存在しない場合は無効であること" do
-      item = Item.new(item_name: nil)
-      item.valid?
-      expect(item.errors[:item_name]).to include("can't be blank")
+      @item.item_name = nil
+      @item.valid?
+      expect(@item.errors[:item_name]).to include("can't be blank")
     end
 
     it "descriptionが存在しない場合は無効であること" do
-      item = Item.new(description: nil)
-      item.valid?
-      expect(item.errors[:description]).to include("can't be blank")
+      @item.description = nil
+      @item.valid?
+      expect(@item.errors[:description]).to include("can't be blank")
     end
     
     it "category_idが1の場合は無効" do
-      item = Item.new(category_id: 1, condition_id: 2, postage_id: 2, prefecture_id: 2, day_id: 2)
-      expect(item).to_not be_valid
+      @item.category_id = 1
+      expect(@item).to_not be_valid
     end
     
     it "condition_idが1の場合は無効" do
-      item = Item.new(category_id: 2, condition_id: 1, postage_id: 2, prefecture_id: 2, day_id: 2)
-      expect(item).to_not be_valid
+      @item.condition_id = 1
+      expect(@item).to_not be_valid
     end
 
     it "postage_idが1の場合は無効" do
-      item = Item.new(category_id: 1, condition_id: 2, postage_id: 2, prefecture_id: 2, day_id: 2)
-      expect(item).to_not be_valid
+      @item.postage_id = 1
+      expect(@item).to_not be_valid
     end
 
     it "prefecture_idが1の場合は無効" do
-      item = Item.new(category_id: 1, condition_id: 2, postage_id: 2, prefecture_id: 2, day_id: 2)
-      expect(item).to_not be_valid
+      @item.prefecture_id = 1
+      expect(@item).to_not be_valid
     end
 
     it "day_idが1の場合は無効" do
-      item = Item.new(category_id: 1, condition_id: 2, postage_id: 2, prefecture_id: 2, day_id: 2)
-      expect(item).to_not be_valid
+      @item.day_id = 1
+      expect(@item).to_not be_valid
     end
 
     it "価格が範囲を下回る場合は無効であること" do
-      item = Item.new(price: 299)
-      item.valid?
-      expect(item.errors[:price]).to include("must be greater than or equal to 300")
+      @item.price = 299
+      @item.valid?
+      expect(@item.errors[:price]).to include("must be greater than or equal to 300")
     end
 
     it "価格が範囲を超える場合は無効であること" do
-      item = Item.new(price: 10000000)
-      item.valid?
-      expect(item.errors[:price]).to include("must be less than or equal to 9999999")
+      @item.price = 10000000
+      @item.valid?
+      expect(@item.errors[:price]).to include("must be less than or equal to 9999999")
     end
   end
  end
